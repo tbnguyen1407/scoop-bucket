53d40f31a1019c559bb199ae477b5a66ed1a453b
Ben Tran <tbnguyen1407@gmail.com>
2024-12-27 18:20:34 +0800

    - bin/auto-pr.ps1: update upstream path;
    Signed-off-by: Ben Tran <tbnguyen1407@gmail.com>

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "tbnguyen1407/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
